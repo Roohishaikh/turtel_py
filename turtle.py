a423426014bd48aa56798e01c585fbcc063e7e75
Roohishaikh <136570437+Roohishaikh@users.noreply.github.com>
2023-06-14 13:27:41 +0530

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,31 @@
+import turtle
+
+pen=turtle.Turtle()
+
+def curve():
+    for i in range(200):
+        pen.right(1)
+        pen.forward(1)
+def heart():
+    pen.fillcolor('red')
+    pen.begin_fill()
+    pen.left(140)
+    pen.forward(113)
+    curve()
+    pen.left(120)
+    curve()
+    pen.forward(112)
+    pen.end_fill()
+
+
+def text1():
+    pen.up()
+    pen.setpos(-20,90)
+    pen.down()
+    pen.color('Black')
+    pen.write("ROOHI")
+
+heart()
+text1()
+pen.ht()
+
